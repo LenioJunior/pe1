b5f675b973e2d1d64cb7f9ceb59473c849302a67
Lênio Oliveira Prado Júnior <lenio@unifei.edu.br>
2023-04-11 15:39:35 -0300

    Soma de 5 numeros

--- repeticao_for.c
+++ repeticao_for.c
@@ -13,9 +13,18 @@ int main(){
     }
 
     for(inc = 0, dec = 30; inc <= 30 && dec >= 0; inc+=2, dec-=3){       
-        printf("Inc: %d\n", inc);
-        printf("Dec: %d\n", dec);
+        // printf("Inc: %d\n", inc);
+        // printf("Dec: %d\n", dec);
+    }
+
+    int soma = 0, cont, num;
+
+    for(cont = 1; cont <= 5; cont++){ 
+        printf("Informe o %d numero: \n", cont);      
+        scanf("%d", &num);
+        soma += num;
     }
     
+    printf("A soma eh: %d", soma);
     return 0;
 }
