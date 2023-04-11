252fe53a7c37106105a7c0140005cc6a0c843c4b
Lênio Oliveira Prado Júnior <lenio@unifei.edu.br>
2023-04-11 15:32:42 -0300

    Implementação efetuada em sala

--- repeticao_for.c
+++ repeticao_for.c
@@ -8,9 +8,14 @@ int main(){
     int inc, dec;
 
     for(inc = 0, dec = 5; inc <= 5 && dec >= 0; inc++, dec--){       
+        //printf("Inc: %d\n", inc);
+        //printf("Dec: %d\n", dec);
+    }
+
+    for(inc = 0, dec = 30; inc <= 30 && dec >= 0; inc+=2, dec-=3){       
         printf("Inc: %d\n", inc);
         printf("Dec: %d\n", dec);
     }
-
+    
     return 0;
 }
